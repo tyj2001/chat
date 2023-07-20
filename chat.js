95bf33036344212cc769c9279905879e7c0734cc
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-07-21 03:42:05 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,84 +3,87 @@ const apiKey = 'purgpt-5bqnjwv8wn3w8lxcctesuk'; // Your actual API Key
 
 // Message handling
 function getChatMessages() {
-  return [...document.getElementById('chat-box').children].map(msgDiv => {
-    const cls = msgDiv.classList[0];
-    const content = msgDiv.innerText.trim();
-    return {role: cls, content: content};
-  });
+    return [...document.getElementById('chat-box').children].map(msgDiv => {
+        const cls = msgDiv.classList[0];
+        const content = msgDiv.innerText.trim();
+        return {role: cls, content: content};
+    });
 }
 
 function appendMessage(role, message) {
-  const msgDiv = document.createElement('div');
-  msgDiv.classList.add(role);
-  msgDiv.innerText = message;
-  document.getElementById('chat-box').appendChild(msgDiv);
+    const msgDiv = document.createElement('div');
+    msgDiv.classList.add(role);
+    msgDiv.innerText = message;
+    document.getElementById('chat-box').appendChild(msgDiv);
 }
 
 // Event handler for the form submission
 document.getElementById('message-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  sendMessage();
+    e.preventDefault();
+    sendMessage();
 });
 
 // Message sending
 async function sendMessage() {
-  const messageInput = document.getElementById('message-input');
-  const userMessage = messageInput.value;
-  if (userMessage.trim() === '') return;
+    const messageInput = document.getElementById('message-input');
+    const userMessage = messageInput.value;
+    if (userMessage.trim() === '') return;
 
-  appendMessage('user', userMessage);
-  messageInput.value = '';
+    appendMessage('user', userMessage);
+    messageInput.value = '';
 
-  const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`
-    },
-    body: JSON.stringify({
-      messages: getChatMessages(),
-      max_tokens: 50, // Adjust as needed
-      temperature: 0.7,
-      top_p: 1,
-      stream: true,
-      stop: "\n",
-      model: 'gpt-3.5-turbo'
-    })
-  });
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            messages: getChatMessages(),
+            max_tokens: 50, // Adjust as needed
+            temperature: 0.7,
+            top_p: 1,
+            stream: true,
+            stop: "\n",
+            model: 'gpt-3.5-turbo'
+        })
+    });
 
-  if (response.ok) {
-    let text = '';
-    let assistantOutput = '';
+    if(response.ok) {
+        let assistantOutput = '';
+        let responseBuffer = '';
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder();
 
-    const reader = response.body.getReader();
-    const decoder = new TextDecoder();
+        while (true) {
+          const { done, value } = await reader.read();
+          if (done) break;
+          const chunk = decoder.decode(value, { stream: true });
 
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) break;
-      text = decoder.decode(value, { stream: true });
-      
-      if (text.startsWith('data: ')) {
-        const jsonStr = text.slice(6); // Trim off the "data: "
-        try {
-          const object = JSON.parse(jsonStr);
-          const message = object['choices'][0]['delta']['content'] || '';
+          responseBuffer += chunk;
 
-          assistantOutput += message;
-          
-        } catch (error) {
-          console.error('JSON parsing error:', error);
+          let newlineIndex;
+          while ((newlineIndex = responseBuffer.indexOf('\n')) !== -1) {
+            const line = responseBuffer.slice(0, newlineIndex).trim();
+            responseBuffer = responseBuffer.slice(newlineIndex + 1);
+
+            if (line.startsWith('data: ')) {
+              const jsonStr = line.slice(6).trim(); // Trim off the "data: "
+              if (!jsonStr.length) continue;
+
+              try {
+                const object = JSON.parse(jsonStr);
+                const message = object['choices'][0]['delta']['content'] || '';
+                assistantOutput += message;
+              } catch (error) {
+                  console.error('JSON parsing error:', error, jsonStr);
+              }
+            }
+          }  
         }
-      } else if (text.trim() === 'data: [DONE]') {
-        break;
-      }
+
+        appendMessage('assistant', assistantOutput);    
+    } else {
+          console.error(`API request failed: ${response.status}`);
     }
-
-    appendMessage('assistant', assistantOutput);
-    
-  } else {
-    console.error(`API request failed: ${response.status}`);
-  }
 }
-
